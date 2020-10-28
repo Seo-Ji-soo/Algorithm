eec801ac3684c9a969ea08ece5003a010d51e629
jisoo <tjwltn999@naver.com>
2020-10-28 15:35:58 +0900

    Create 1012.java

---
+++ 1000/1012.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		Scanner sc = new Scanner(System.in);
+		
+		float n;
+		n=sc.nextFloat();
+		
+		System.out.printf("%6f", n);
+	}
+
+}
+
+
