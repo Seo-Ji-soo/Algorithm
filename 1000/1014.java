6e1f14ce0627294fc1ae5856ece7568bb22e7625
jisoo <tjwltn999@naver.com>
2020-10-29 17:08:40 +0900

    Create 1014.java

---
+++ 1000/1014.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		Scanner sc = new Scanner(System.in);
+		
+		String a,b;
+		
+		a=sc.next();
+		b=sc.next();
+		
+		System.out.printf("%s %s",b, a);
+		
+	}
+
+}
