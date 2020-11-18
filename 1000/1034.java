b88424d96588cba68df5c80586db63d6c058d0be
jisoo <tjwltn999@naver.com>
2020-11-18 16:31:22 +0900

    Create 1034.java

---
+++ 1000/1034.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		
+		int b = Integer.valueOf(a, 8);
+		
+		System.out.print(b);
+		
+		
+
+	}
+}
