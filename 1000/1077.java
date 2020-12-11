2a71a133efbc426ff4c20600d61e70d1ac2c4f0d
jisoo <tjwltn999@naver.com>
2020-12-11 21:53:25 +0900

    Create 1077.java

---
+++ 1000/1077.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int start;
+		int a = sc.nextInt();
+		
+		for(start=0; start<=a; start++) {
+			System.out.println(start);
+		}
+    }
+}	
+
+	
