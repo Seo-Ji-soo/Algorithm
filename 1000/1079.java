d86be9695d4c638de726f9b1616c4d7850ca8ffa
jisoo <tjwltn999@naver.com>
2020-12-13 19:05:31 +0900

    Create 1079.java

---
+++ 1000/1079.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+
+		
+		while(true) {
+			char a = sc.next().charAt(0);
+			if(a == 'q') {
+				System.out.println(a);
+				break;
+			}
+			System.out.println(a);
+		}	
+
+    }
+}	
+
+
