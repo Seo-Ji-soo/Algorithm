e2322db9343807965fcce67f17ed98fca3fd550d
jisoo <tjwltn999@naver.com>
2021-01-16 03:36:38 +0900

    Create 수학2_3053.java

---
+++ Baekjoon/수학2_3053.java
@@ -0,0 +1,24 @@
+// 유클리드 기하학과 택시 기하학에 대한 문제
+// 택시 기하학 부분은 이해하기 어려워 공식을 보고 코딩 작성
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double r = sc.nextInt();
+		
+		// Math.PI : 원주율 구하는 함수. EX) 3.14~~~
+		System.out.println(String.format("%.6f", r*r*Math.PI));
+		System.out.printf(String.format("%.6f", r*r*2));
+
+
+		
+			   }//메인메소드종료
+
+	
+		}//클래스종료
+
